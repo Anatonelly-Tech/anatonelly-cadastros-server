2ecad8e9f83910e83bde6b0468200746e902bc03
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-05-31 11:10:54 -0300

    fix: remove account

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 const driverRouter = require('./driver');
-const accountRouter = require('./account');
 
-router.use('/', accountRouter);
 router.use('/', driverRouter);
 
 module.exports = router;
